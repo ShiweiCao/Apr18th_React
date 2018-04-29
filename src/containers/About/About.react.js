eca9edeb39f6a34aaa1683da0bc585aaaf63f8d9
ShiweiCao <caoshiwei93@gmail.com>
2018-04-28 18:01:19 -0700

    2nd merge

--- src/containers/About/About.react.js
+++ src/containers/About/About.react.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Avatar from 'material-ui/Avatar';
 
 import './About.css';
 
@@ -16,6 +17,10 @@ export default class About extends Component{
                         This is the project we made in Apr.28th Hackthon.
                         <br/>
                         <h3> Team member </h3>
+                        <div className="avatars">    
+                            <Avatar src="images/SC.jpg" size={100} />
+                            <Avatar src="images/TY.jpg" size={100} />
+                        </div>
                     </CardText>
                 </Card>
                 </MuiThemeProvider>
